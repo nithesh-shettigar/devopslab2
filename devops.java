add1cd1640b5db1711107b51b6a10589cc4df553
Nikhilra0 <nikhilra09945@gmail.com>
2021-10-28 15:12:28 +0530

    second commit

--- devops.java
+++ devops.java
@@ -15,7 +15,8 @@ for(j=0; j<=i; j++)
 System.out.print("* ");   
 }   
 //throws the cursor in a new line after printing each line  
-System.out.println();   
+System.out.println("hello  world"); 
+  
 }   
 }   
 }  
